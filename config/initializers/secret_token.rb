3ea02ad9b76022ab6dcd20d2abd53bee11c8c137
jackytugit <jacky.tu@gmail.com>
2014-02-14 16:06:44 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BikeShopsNa::Application.config.secret_key_base = '443dc83e31802cd7bc862d8760d66caac8f2055781fc4c95f00a91543aede0d58fdfd6b73b0dcbd43c2a7b32609ce9d854103aeceb2351c73ee1accb1d5da5d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BikeShopsNa::Application.config.session_store :cookie_store, key: '_bike_shops_na_session'
